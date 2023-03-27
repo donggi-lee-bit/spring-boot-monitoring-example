d0ddeca9aa0206d7d5ae956e3d2e8a77858b3bf9
donggi-lee-bit <devdonggilee@gmail.com>
2023-03-27 20:49:12 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/example/springbootmonitoringexample/SpringBootMonitoringExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmonitoringexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMonitoringExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMonitoringExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootmonitoringexample/SpringBootMonitoringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmonitoringexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMonitoringExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
